1266abdce4311cf5ec751153eddef83ca8ba9647
sdp8554 <ihurtmyknee11@gmail.com>
2025-04-25 02:23:15 -0400

    Generated The Parse Crossword Template Function

--- AndroidAppDev/app/src/main/java/com/example/crosswordandroidapp/Crossword.kt
+++ AndroidAppDev/app/src/main/java/com/example/crosswordandroidapp/Crossword.kt
@@ -1,46 +1,43 @@
 package com.example.crosswordandroidapp
 
+import android.util.Log
 import androidx.core.text.isDigitsOnly
+import java.util.Dictionary
+import java.util.HashMap
 
 
 class Crossword(val row: Int, val col: Int) {
     data class CrosswordCell(val cellType: String, val cellContent: Char = ' ')
     private var gridData : ArrayList<CrosswordCell> = ArrayList()
+    private var cluesAcross = hashMapOf<Int, String>()
+    private var cluesDown = hashMapOf<Int, String>()
 
-    
-    
+    fun setCluesAcross(hashMap: HashMap<Int,String>){
+        cluesAcross = hashMap
+    }
+    fun setCluesDown(hashMap: HashMap<Int,String>){
+        cluesDown = hashMap
+    }
+
     //Pass in the format string(line 2 of the txt) to populate the blank crossword
     //Returns 0 if success, other for failure
     //Modifies: gridData
     fun populateGridData(fileString: String): Int {
         val subStrings = fileString.split(",")
-            for (i in 0..<(row * col)){
-                val currentVal = subStrings[i]
-                val currentCell : CrosswordCell;
-                if (currentVal == "X"){//Black Square
+            for (element in subStrings){
+                val currentCell : CrosswordCell
+                if (element == "X") {//Black Square
                     currentCell = CrosswordCell("Black")
-                }
-                else if (currentVal == "O") {//Empty square for entering
+                } else if (element == "O") {//Empty square for entering
                     currentCell = CrosswordCell("Empty")
-                }
-                else if (currentVal.isDigitsOnly()){//clue tile
-                    currentCell = CrosswordCell(currentVal)
-                }
-                else{
+                } else if (element.isDigitsOnly()) {//clue tile
+                    currentCell = CrosswordCell(element)
+                } else {
                     println("Invalid Grid Format String Provided")
-                    return 1;
+                    return 1
                 }
-                gridData[i] = currentCell;
+                gridData.add(currentCell)
             }
-        return 0;
-    }
-    
-    fun printGridData(){
-        for (i in 0..<gridData.size){
-            print(gridData[i].cellType)
-            if (i%(row-1) == 0){
-                println()
-            }
-        }
+        return 0
     }
 }
--- AndroidAppDev/app/src/main/java/com/example/crosswordandroidapp/MainActivity.kt
+++ AndroidAppDev/app/src/main/java/com/example/crosswordandroidapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.crosswordandroidapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,11 +13,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.crosswordandroidapp.ui.theme.CrosswordAndroidAppTheme
+import java.io.BufferedReader
 import java.io.File
+import java.util.HashMap
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //parseCrosswordFile(this,"testFileFormat.txt")
         enableEdgeToEdge()
         setContent {
             CrosswordAndroidAppTheme {
@@ -39,9 +43,63 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-fun parseCrosswordFile(fileName: String){
-    val file = File(fileName)
 
+
+fun parseCrosswordFile(context: Context ,fileName: String): Int {
+    val inputStream = context.assets.open(fileName)
+    val fileBuffer = inputStream.bufferedReader()
+    //Process Crossword grid dimension (line 1) should be Col then Row
+    var currLineString:String = fileBuffer.readLine()
+    val parseStr: List<String> = currLineString.split(" ")
+    if (parseStr.size != 2){
+        println("Invalid matrix dimension $parseStr")
+        return 1
+    }
+    val crossword = Crossword(parseStr[1].toInt(), parseStr[0].toInt())
+
+    //Process Grid Template
+    currLineString = fileBuffer.readLine()
+    crossword.populateGridData(currLineString)
+
+    //Process Across Clues
+    currLineString = fileBuffer.readLine()
+    if (currLineString == "ACROSS"){
+        var acrossHash = hashMapOf<Int,String>()
+        while(true){
+            currLineString = fileBuffer.readLine()
+            if (currLineString == "END_ACROSS"){
+                break;
+            }
+            parseClueString(currLineString,acrossHash)
+        }
+        crossword.setCluesAcross(acrossHash)
+    }else{
+        print("Invalid file format: expected across got $currLineString")
+        return 1
+    }
+
+    //Process Down Clues
+    currLineString = fileBuffer.readLine()
+    if (currLineString == "DOWN"){
+        var downHash = hashMapOf<Int,String>()
+        while(true){
+            currLineString = fileBuffer.readLine()
+            if (currLineString == "END_DOWN"){
+                break
+            }
+            parseClueString(currLineString,downHash)
+        }
+        crossword.setCluesDown(downHash)
+    }else{
+        print("Invalid file format: expected across got $currLineString")
+        return 1
+    }
+    return 0
+}
+
+fun parseClueString(inputString: String, hashMapToPop: HashMap<Int,String>){
+    val processedString: List<String> = inputString.split(":", limit = 2)
+    hashMapToPop[processedString[0].toInt()] = processedString[1]
 }
 
 @Preview(showBackground = true)
