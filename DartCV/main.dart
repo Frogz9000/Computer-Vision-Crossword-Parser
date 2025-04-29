d149294b7c450bc58250bcfabd6fa3a08de857da
sdp8554 <ihurtmyknee11@gmail.com>
2025-04-28 23:04:57 -0400

    set up flutter infastructure

---
+++ DartCV/main.dart
@@ -0,0 +1,7 @@
+/*TODO Convert main.py to this
+flutter install guide -> https://docs.flutter.dev/get-started/install
+These are from cursory search, there may be better packages to do this with
+OCR tool ->  https://pub.dev/packages/google_ml_kit
+OpenCV -> https://pub.dev/packages/opencv_dart
+Numpy -> https://github.com/jsonfm/numpy-dart
+*/
